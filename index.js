d3a331b75243aa8d89e0ab05575221abfe20ea42
Kevin Rodd <roddkevin@gmail.com>
2023-09-18 20:55:01 +0200

    Update index.js
    - added DE Locale support
    - added better console.log output

--- index.js
+++ index.js
@@ -1,38 +1,43 @@
-const yargs = require('yargs');
+const yargs = require("yargs");
 
-const helloFresh = require('./services/hello-fresh');
+const helloFresh = require("./services/hello-fresh");
 
 // Parse command-line configurations
 const argv = yargs
   .strict(true)
-  .usage('Usage: $0 <command> [options]')
-  .command('HelloFresh', 'Perform crawling on HelloFresh', {
+  .usage("Usage: $0 <command> [options]")
+  .command("HelloFresh", "Perform crawling on HelloFresh", {
     locale: {
-      alias: 'l',
-      describe: 'Locale to perform crawling on.',
-      choices: ['US', 'GB'],
-      default: 'US',
+      alias: "l",
+      describe: "Locale to perform crawling on.",
+      choices: ["US", "GB", "DE"],
+      default: "US",
       nargs: 1,
     },
     recipeCardSaveDirectory: {
-      alias: 's',
-      describe: 'Directory where to save PDF recipe cards.',
-      default: './recipe-card-pdfs',
+      alias: "s",
+      describe: "Directory where to save PDF recipe cards.",
+      default: "./recipe-card-pdfs",
       nargs: 1,
     },
   })
-  .demandCommand(1, 1, 'Please specify which service should be used')
-  .example('$0 HelloFresh -l GB -s ./downloads', 'Crawl on HelloFresh and use the GB locale').argv;
+  .demandCommand(1, 1, "Please specify which service should be used")
+  .example(
+    "$0 HelloFresh -l GB -s ./downloads",
+    "Crawl on HelloFresh and use the GB locale"
+  ).argv;
 
-console.log(`Performing crawl on ${argv._} using ${argv.locale} locale`);
+console.log(
+  `\x1b[32m Performing crawl on ${argv._} using ${argv.locale} locale \x1b[0m`
+);
 
-if (argv._[0] === 'HelloFresh') {
+if (argv._[0] === "HelloFresh") {
   helloFresh
     .crawl(argv)
     .catch(console.error)
     .finally(() => {
-      console.log('The crawling process has completed. Press enter to exit...');
+      console.log("The crawling process has completed. Press enter to exit...");
       process.stdin.resume();
-      process.stdin.on('data', process.exit.bind(process, 0));
+      process.stdin.on("data", process.exit.bind(process, 0));
     });
 }
